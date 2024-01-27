4f413584e1726d9f3514d458117c1d51f6d6214c
Dexterski007 <mbouj007@gmail.com>
2024-01-27 10:20:21 -0800

    hey

--- factors.c
+++ factors.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[]) {
     FILE *file_stream;
