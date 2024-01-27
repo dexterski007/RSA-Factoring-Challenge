94220220b8de83a82d46a65c5538a2b46424b082
Dexterski007 <mbouj007@gmail.com>
2024-01-27 10:40:29 -0800

    hey

--- factors.c
+++ factors.c
@@ -1,46 +1,83 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
-{
-	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	long long flag = 1, div, rest, number, counter;
-	ssize_t nread;
+typedef struct {
+    long long prime;
+    int power;
+} Factor;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+bool is_prime(long long n) {
+    if (n <= 1) return false;
+    if (n <= 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+    for (long long i = 5; i * i <= n; i += 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    return true;
+}
 
-	file = fopen(argv[1], "r");
-	if (file == NULL) {
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
+void factorize(long long n, Factor factors[], int* num_factors) {
+    *num_factors = 0;
+    if (n <= 1) return;
 
-	while ((nread = getline(&line, &len, file)) != -1) {
-		flag = 1, div = 2;
-		number = atoll(line);
-		while (flag) {
-			rest = number % div;
-			if (!rest) {
-				counter = number / div;
-				printf("%lld=%lld*%lld\n", number, counter, div);
-				flag = 0;
-			}
-			div++;
-		}
-	}
+    // Factorization using trial division
+    for (long long i = 2; i * i <= n; i++) {
+        while (n % i == 0) {
+            if (*num_factors < 2) {
+                factors[*num_factors].prime = i;
+                factors[*num_factors].power = 1;
+                (*num_factors)++;
+            } else {
+                factors[0].power *= factors[1].prime;
+                factors[1] = factors[*num_factors - 1];
+                (*num_factors)--;
+            }
+            n /= i;
+        }
+    }
+    if (n > 1) {
+        if (*num_factors < 2) {
+            factors[*num_factors].prime = n;
+            factors[*num_factors].power = 1;
+            (*num_factors)++;
+        } else {
+            factors[0].power *= factors[1].prime;
+            factors[1].prime = n;
+        }
+    }
+}
 
-	free(line);
-	fclose(file);
-	exit(EXIT_SUCCESS);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* file = fopen(argv[1], "r");
+    if (!file) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    long long number;
+    while (fscanf(file, "%lld", &number) != EOF) {
+        Factor factors[2];
+        int num_factors;
+
+        factorize(number, factors, &num_factors);
+
+        printf("%lld=", number);
+        for (int i = 0; i < num_factors; i++) {
+            printf("%lld", factors[i].prime);
+            if (i < num_factors - 1) {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    fclose(file);
+    return 0;
 }
