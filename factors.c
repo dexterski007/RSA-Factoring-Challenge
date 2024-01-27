e3cead16bdaf185c5b08fb148bd54c9c8d8d3c04
Dexterski007 <mbouj007@gmail.com>
2024-01-27 10:22:05 -0800

    hey

--- factors.c
+++ factors.c
@@ -1,48 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <math.h>
 #include <string.h>
 
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char *line = NULL;
+	size_t len = 0;
+	long long flag = 1, div, rest, number, counter;
+	ssize_t nread;
 
-int main(int argc, char *argv[]) {
-    FILE *file_stream;
-    char *current_line = NULL;
-    size_t line_length = 0;
-    long long is_factor_found = 1, divisor, remainder, current_number, factor;
-    ssize_t chars_read;
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	if (file == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
 
-    file_stream = fopen(argv[1], "r");
-    if (file_stream == NULL) {
-        perror("fopen");
-        exit(EXIT_FAILURE);
-    }
+	while ((nread = getline(&line, &len, file)) != -1) {
+		flag = 1, div = 2;
+		number = atoll(line);
+		while (flag) {
+			rest = number % div;
+			if (!rest) {
+				counter = number / div;
+				printf("%lld=%lld*%lld\n", number, counter, div);
+				flag = 0;
+			}
+			div++;
+		}
+	}
 
-    while ((chars_read = getline(&current_line, &line_length, file_stream)) != -1) {
-        is_factor_found = 1;
-        divisor = 2;
-        current_number = atoll(current_line);
-
-        while (is_factor_found) {
-            remainder = current_number % divisor;
-            if (!remainder) {
-                factor = current_number / divisor;
-                printf("%lld=%lld*%lld\n", current_number, factor, divisor);
-                is_factor_found = 0;
-            }
-            divisor++;
-        }
-    }
-
-    free(current_line);
-    fclose(file_stream);
-    exit(EXIT_SUCCESS);
+	free(line);
+	fclose(file);
+	exit(EXIT_SUCCESS);
 }
