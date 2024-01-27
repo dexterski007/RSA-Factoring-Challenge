68ef73eaec6b5fd300ae0c2725a8e1d6653066ad
Dexterski007 <mbouj007@gmail.com>
2024-01-27 10:18:45 -0800

    fac

--- factors.c
+++ factors.c
@@ -1,46 +1,41 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>
-#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-	FILE *stream;
-	char *line = NULL;
-	size_t len = 0;
-	long long flag = 1, div, rest, number, counter;
-	ssize_t nread;
+int main(int argc, char *argv[]) {
+    FILE *file_stream;
+    char *current_line = NULL;
+    size_t line_length = 0;
+    long long is_factor_found = 1, divisor, remainder, current_number, factor;
+    ssize_t chars_read;
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
-	stream = fopen(argv[1], "r");
-	if (stream == NULL) {
-		perror("fopen");
-		exit(EXIT_FAILURE);
-	}
+    file_stream = fopen(argv[1], "r");
+    if (file_stream == NULL) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
 
-	while ((nread = getline(&line, &len, stream)) != -1) {
-		flag = 1, div = 2;
-		number = atoll(line);
-		while (flag) {
-			rest = number % div;
-			if (!rest) {
-				counter = number / div;
-				printf("%lld=%lld*%lld\n", number, counter, div);
-				flag = 0;
-			}
-			div++;
-		}
-	}
+    while ((chars_read = getline(&current_line, &line_length, file_stream)) != -1) {
+        is_factor_found = 1;
+        divisor = 2;
+        current_number = atoll(current_line);
 
-	free(line);
-	fclose(stream);
-	exit(EXIT_SUCCESS);
+        while (is_factor_found) {
+            remainder = current_number % divisor;
+            if (!remainder) {
+                factor = current_number / divisor;
+                printf("%lld=%lld*%lld\n", current_number, factor, divisor);
+                is_factor_found = 0;
+            }
+            divisor++;
+        }
+    }
+
+    free(current_line);
+    fclose(file_stream);
+    exit(EXIT_SUCCESS);
 }
